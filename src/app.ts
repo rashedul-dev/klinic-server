3336e608e1aee776945dc4b69c2a8c36a17f01ed
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-19 15:38:35 +0600

    add: Creating Patient (User)

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Ph health care server..",
+  });
 });
-
 
 app.use(globalErrorHandler);
 
