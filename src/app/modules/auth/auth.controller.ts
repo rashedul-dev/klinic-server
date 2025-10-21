ad65069207a25931d39fa1fdb086f42ecac9b314
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-21 12:14:18 +0600

    add: Implement User Login

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { AuthService } from "./auth.services";
+
+const login = catchAsync(async (req: Request, res: Response) => {
+  const result = await AuthService.login(req.body);
+  // console.log(req.body);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "User login successfully",
+    data: result,
+  });
+});
+export const AuthController = {
+  login,
+};
