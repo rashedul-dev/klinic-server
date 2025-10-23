c6b71677cb71002c53cdbfa52d6d967223aaa2a9
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-24 01:24:15 +0600

    add: pagination helper logic

---
+++ src/app/helper/paginationHelper.ts
@@ -0,0 +1,27 @@
+export type IOptions = {
+  page?: string | number;
+  limit?: string | number;
+  sortBy?: string;
+  sortOrder?: string;
+};
+export type IOptionResult = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+};
+
+const calculatePagination = (options: IOptions): IOptionResult => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip: number = Number(page - 1) * limit;
+
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+  return { page, limit, skip, sortBy, sortOrder };
+};
+
+export const paginationHelper = {
+  calculatePagination,
+};
