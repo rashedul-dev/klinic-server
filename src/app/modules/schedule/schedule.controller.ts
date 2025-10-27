cfda8f60045fee4b9e0307468640603bdeadba34
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-27 15:00:20 +0600

    add: delete schedule from DB controller

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -4,6 +4,7 @@ import { ScheduleService } from "./schedule.service";
 import sendResponse from "../../shared/sendResponse";
 import pick from "../../helper/pick";
 import { filterByStartAndEndDate, schedulePagination } from "./schedule.constant";
+import { IJWTPaylaod } from "../../types/common";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const result = await ScheduleService.insertIntoDB(req.body);
@@ -15,11 +16,12 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-const schedulesForDoctor = catchAsync(async (req: Request, res: Response) => {
+const schedulesForDoctor = catchAsync(async (req: Request & { user?: IJWTPaylaod }, res: Response) => {
   const filters = pick(req.query, filterByStartAndEndDate);
   const options = pick(req.query, schedulePagination);
 
-  const result = await ScheduleService.scheduleForDoctor(filters, options);
+  const user = req.user;
+  const result = await ScheduleService.scheduleForDoctor(user as IJWTPaylaod, filters, options);
 
   sendResponse(res, {
     statusCode: 200,
@@ -28,8 +30,19 @@ const schedulesForDoctor = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const deleteSchedule = catchAsync(async (req: Request, res: Response) => {
+  const result = await ScheduleService.deleteSchedule(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Schedule Deleted Successfully",
+    data: result,
+  });
+});
 
 export const ScheduleController = {
   insertIntoDB,
   schedulesForDoctor,
+  deleteSchedule,
 };
