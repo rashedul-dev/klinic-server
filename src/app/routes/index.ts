1e862a4541bb0511ae396351f679bf2fded07644
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-28 14:46:37 +0600

    add: create routes  of doctors specialties

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { UserRoutes } from "../modules/user/user.routes";
 import { AuthRoutes } from "../modules/auth/auth.routes";
 import { ScheduleRoutes } from "../modules/schedule/schedule.routes";
-import { doctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.routes";
+import { DoctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.routes";
+import { SpecialtiesRoutes } from "../modules/specialties/specialties.routes";
 
 const router = express.Router();
 
@@ -21,8 +22,16 @@ const moduleRoutes = [
   },
   {
     path: "/doctor-schedule",
-    route: doctorScheduleRoutes,
+    route: DoctorScheduleRoutes,
   },
+  {
+    path: "/specialties",
+    route: SpecialtiesRoutes,
+  },
+  // {
+  //   path: "/doctor",
+  //   route: DoctorRoutes,
+  // },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
