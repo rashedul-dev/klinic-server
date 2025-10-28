5857cf283a271a8a02942777b78e966c20de10ab
rashedul-dev <rashedulislam.edge@gmail.com>
2025-10-29 01:05:15 +0600

    add: get all doctor and update doctor info controller

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import pick from "../../helper/pick";
+import { doctorFilterableFields, doctorPagination } from "./doctor.constant";
+import { DoctorService } from "./doctor.service";
+import sendResponse from "../../shared/sendResponse";
+
+const getAllDoctor = catchAsync(async (req: Request, res: Response) => {
+  const options = pick(req.query, doctorPagination);
+  const fillters = pick(req.query, doctorFilterableFields);
+
+  const result = await DoctorService.getAllDoctor(fillters, options);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor retrived successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const updateDoctor = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const result = await DoctorService.getAllDoctor(id, req.body);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Doctor updated successfully!",
+    data: result,
+  });
+});
+
+export const DoctorController = {
+  getAllDoctor,
+  updateDoctor,
+};
