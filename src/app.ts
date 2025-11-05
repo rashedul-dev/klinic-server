f66c9c014c1a33a4c91e77dcedb47cc2bc52b721
rashedul-dev <rashedulislam.edge@gmail.com>
2025-11-06 01:12:09 +0600

    feat(payment): add stripe payment webhook in the app route

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 import cookieParser from "cookie-parser";
+import { PaymentController } from "./app/modules/payment/payment.controller";
 
 const app: Application = express();
+
+app.use(
+  "/api/v1/payments/webhook",
+  express.raw({ type: "application/json" }),
+  PaymentController.handleStripeWebhookEvent
+);
+
+// Other routes after express.json()
+app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
