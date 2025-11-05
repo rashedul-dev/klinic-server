0d2b6c048222d715abe183abd9adc5435d198587
rashedul-dev <rashedulislam.edge@gmail.com>
2025-11-06 00:58:54 +0600

    feat: implement payment controller with payment processing endpoints

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { PaymentService } from "./payment.service";
+import sendResponse from "../../shared/sendResponse";
+import stripe from "../../shared/stripe";
+import config from "../../../config";
+
+const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+  const sig = req.headers["stripe-signature"] as string;
+  // const webhookSecret =config.stripe_webHook_secret;
+  const webhookSecret = "whsec_9573e1a238462bdb506ccfab34dbb9a9cadb408a9e3eefa4759dece8a7590ea5";
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret as string);
+  } catch (err: any) {
+    console.error("⚠️ Webhook signature verification failed:", err.message);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+  const result = await PaymentService.handleStripeWebhookEvent(event);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Webhook req send successfully",
+    data: result,
+  });
+});
+
+export const PaymentController = {
+  handleStripeWebhookEvent,
+};
