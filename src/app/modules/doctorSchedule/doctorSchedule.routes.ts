6d5e5664e259b60755410ffe3652400f50d9240b
rashedul-dev <rashedulislam.edge@gmail.com>
2025-11-16 00:00:40 +0600

    feat(doctor-schedule): set up doctor schedule routes with role-based auth

--- src/app/modules/doctorSchedule/doctorSchedule.routes.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.routes.ts
@@ -7,11 +7,22 @@ import { DoctorScheduleValidation } from "./doctorSchedule.validation";
 
 const router = express.Router();
 
+// ADMIN ONLY ROUTES
+router.get("/", auth(UserRole.ADMIN), DoctorScheduleController.getAllDoctorSchedules);
+
+// DOCTOR ONLY ROUTES
+router.get("/my-schedules",
+   auth(UserRole.DOCTOR), DoctorScheduleController.getMySchedules);
+
+// CREATE DOCTOR SCHEDULE
 router.post(
   "/",
   auth(UserRole.DOCTOR),
   validateRequest(DoctorScheduleValidation.createDoctorScheduleValidationSchema),
   DoctorScheduleController.createDoctorSchedule
 );
+
+// DELETE DOCTOR SCHEDULE
+router.delete("/:id", auth(UserRole.DOCTOR), DoctorScheduleController.deleteSchedule);
 
 export const DoctorScheduleRoutes = router;
